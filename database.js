3f0db58254b4d00590a85d7c477343a2252a5c88
manojkumar0100 <129994758+manojkumar0100@users.noreply.github.com>
2024-01-02 21:34:20 +0530

    Update database.js

--- database.js
+++ database.js
@@ -3,15 +3,15 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   
 
-  // host: 'localhost',
-  // user: 'root',
-  // database: 'flex',
-  // port     : '3306'
-   host     : 'bcz0rqlcszpmyxwajhoy-mysql.services.clever-cloud.com',
-  user     : 'uudiwh6yoyi3ed5n',
-   password : '6BGzV8wIabqLJFBKZkQi',
-  database : 'bcz0rqlcszpmyxwajhoy',
+  host: 'localhost',
+  user: 'root',
+  database: 'flex',
   port     : '3306'
+  //  host     : 'bcz0rqlcszpmyxwajhoy-mysql.services.clever-cloud.com',
+  // user     : 'uudiwh6yoyi3ed5n',
+  //  password : '6BGzV8wIabqLJFBKZkQi',
+  // database : 'bcz0rqlcszpmyxwajhoy',
+  // port     : '3306'
   
   
 });
